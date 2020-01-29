6774f068a03db62d85badb480ab8fc1560a26b52
Patrick McCorry <stonecoldpat@gmail.com>
2020-01-29 19:31:16 +0000

    Update Ballot.sol

--- src/contracts/Ballot.sol
+++ src/contracts/Ballot.sol
@@ -4,9 +4,7 @@ import "./MetaTransaction.sol";
 
 /// @title Voting with delegation.
 contract Ballot is MetaTransaction {
-    // This declares a new complex type which will
-    // be used for variables later.
-    // It will represent a single voter.
+
     struct Voter {
         uint weight; // weight is accumulated by delegation
         bool voted;  // if true, that person already voted
@@ -21,18 +19,14 @@ contract Ballot is MetaTransaction {
     }
 
     address public chairperson;
-
-    // This declares a state variable that
-    // stores a \`Voter\` struct for each possible address.
     mapping(address => Voter) public voters;
-
-    // A dynamically-sized array of \`Proposal\` structs.
+    
     Proposal[] public proposals;
 
     event VoteRecorded(uint proposal, address voter);
     event RegisteredVoter(address voter);
 
-    /// Create a new ballot to choose one of \`proposalNames\`.
+    /// Create a new ballot to choose one of proposalNames`.
     constructor(bytes32[] memory proposalNames) public {
         chairperson = msg.sender;
         voters[chairperson].weight = 1;
@@ -41,9 +35,6 @@ contract Ballot is MetaTransaction {
         // create a new proposal object and add it
         // to the end of the array.
         for (uint i = 0; i < proposalNames.length; i++) {
-            // \`Proposal({...})\` creates a temporary
-            // Proposal object and \`proposals.push(...)\`
-            // appends it to the end of \`proposals\`.
             proposals.push(Proposal({
                 name: proposalNames[i],
                 voteCount: 0
@@ -51,8 +42,8 @@ contract Ballot is MetaTransaction {
         }
     }
 
-    // Give \`voter\` the right to vote on this ballot.
-    // May only be called by \`chairperson\`.
+    // Give voter the right to vote on this ballot.
+    // May only be called by chairperson.
     function giveRightToVote(address voter) public {
         require(msg.sender == chairperson, "Only chairman can approve new voters");
         require(!voters[voter].voted, "Voter has already voted");
@@ -68,13 +59,13 @@ contract Ballot is MetaTransaction {
     }
 
     /// Give your vote (including votes delegated to you)
-    /// to proposal \`proposals[proposal].name\`.
+    /// to proposal proposals[proposal].name
     function castVote(uint proposal, address voter) internal {
         require(!voters[voter].voted, "Voter has already voted");
         voters[voter].voted = true;
         voters[voter].vote = proposal;
 
-        // If \`proposal\` is out of the range of the array,
+        // If proposal is out of the range of the array,
         // this will throw automatically and revert all
         // changes.
         proposals[proposal].voteCount += voters[voter].weight;
